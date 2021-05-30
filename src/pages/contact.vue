77ce08703fac213bddd30952c3654085828b0d02
KarnTNI <84868937+KarnTNI@users.noreply.github.com>
2021-05-30 15:46:47 +0700

    Initial commit

---
+++ src/pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="p-8">
+    <figure class="bg-gray-100 rounded-xl p-8 shadow-md">
+      <div class="pt-6 text-center space-y-4">
+        <figcaption class="font-medium">
+          <div class="text-gray-500 text-center">
+            <li>tel : {{ tel }}</li>
+            <li>email : {{ email }}</li>
+            <li>facebook : {{ facebook }}</li>
+            <li>line : {{ line }}</li>
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: "Karnye555@gmail.com",
+    facebook: "Karn Leelagate",
+    tel: "097-146-7555",
+    line: "Yuthaaf000",
+  }),
+};
+</script>
---
+++ src/pages/info.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="p-8">
+    <figure class="xl:flex bg-gray-100 rounded-xl p-8 shadow-md">
+      <div class="pt-6 text-center space-y-4">
+        <div class="text-cyan-500">Name : {{ name }} {{ surname }}</div>
+        <div class="text-cyan-500">Age : {{ age }}</div>
+        <figcaption class="font-medium">
+          <div class="text-cyan-500">University : {{ university }}</div>
+          <div class="text-cyan-500">Faculty : {{ faculty }}</div>
+          <div class="break-words text-gray-500">
+            Skills :
+            <span v-for="skill in skills" :key="skill">
+              {{ skill }}
+              <span v-if="skill != skills[skills.length - 1]">, </span>
+            </span>
+          </div>
+          <div class="pt-6">
+            <router-link :to="{ name: 'contact' }">
+              <a class="text-gray-500 hover:text-yellow-500">
+                <strong>Contact>></strong>
+              </a>
+            </router-link>
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: "Karn",
+    surname: "Leelagate",
+    age: 20,
+    university: "Thai-Nichi Institute of Technology",
+    faculty: "BI",
+    skills: ["Java", "ABAP", "PL/SQL", "SQL"],
+  }),
+};
+</script>
